4a4220aa0e5901ebc6d37ffad552ae71afeb9be8
Jhonathan Salas <jhonathan.salas@globant.com>
2022-08-03 19:46:37 -0500

    Actualizando repos posgres y redis

---
+++ postgre/postgre_rest.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	FirstName string   `json:"firstname,omitempty"`
+	LastName  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+// Stores the data received from the client.
+var people []Person
+
+// Browser response - Request from the client
+func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	var person Person
+	_ = json.NewDecoder(req.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+func UpdatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	var person Person
+	_ = json.NewDecoder(req.Body).Decode(&person)
+	params := mux.Vars(req)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people[index] = person
+			json.NewEncoder(w).Encode(person)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(people)
+}
+
+func main() {
+	router := mux.NewRouter()
+	// Data for test
+	people = append(people, Person{ID: "1", FirstName: "Ryan", LastName: "Ray", Address: &Address{City: "Dubling", State: "California"}})
+	people = append(people, Person{ID: "2", FirstName: "Joe", LastName: "McMillan"})
+	// Endpoints
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", UpdatePersonEndpoint).Methods("PUT")
+	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
+	// Start server
+	fmt.Println("Starting server - Port: 3000")
+	// In case of an error, handle it with log.
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
--- redis.go
+++ redis.go
@@ -1,33 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 
-	"github.com/go-redis/redis"
+	"google.golang.org/genproto/googleapis/cloud/redis/v1"
 )
 
-func() {
+type Employee struct {
+	Name    string `json:"name`
+	Address string `json:"address`
+}
+
+func main() {
 
 	// new redis client
 	client := redis.NewClient(&redis.Options{
 
-		Addr: "127.0.0.1:6379",
-
+		Addr:     "localhost:6000",
 		Password: "",
-
-		DB: 10,
+		DB:       0,
 	})
 
-	// test connection
-	pong, err := client.Ping().Result()
-
+	json, err := json.Marshal(Employee{Name: "Gordon", Address: "San Jose"})
 	if err != nil {
-
-		log.Fatal(err)
-
+		fmt.Println(err)
 	}
 
-	// return pong if server is online
-	fmt.Println(pong)
+	err = client.Set("add123", json, 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	val, err := client.Get("add123").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(val)
 }
